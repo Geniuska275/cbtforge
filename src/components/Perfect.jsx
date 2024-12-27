c426bbddbadf94dde0f9585728c0736b146c54fb
Geniuska275 <aigbojie2020@gmail.com>
2024-12-27 22:11:31 +0100

    updated

--- src/components/Perfect.jsx
+++ src/components/Perfect.jsx
@@ -9,7 +9,7 @@ function Perfect() {
 <p className="text-white mt-2 font-bold">Discover our comprehensive range of CBT products designed to meet your unique needs.</p>
 <div className="flex items-center gap-3">
 <input type="email" className=" btn px-10 py-2 mt-3 border border-[#7EC5FF] rounded-lg border-2 border-gray-300 focus:outline-none focus:border-[#7EC5FF]" placeholder="Enter Your Email..."/>
-  <button className=" btnss h-[40px] w-[200px] bg-[#7EC5FF]  rounded-xl font-bold mt-3 hover:border hover:text-white hover:bg-black">TRY 4 FREE</button>
+  <button className=" per h-[40px] w-[200px] bg-[#7EC5FF]  rounded-xl font-bold mt-3 hover:border hover:text-white hover:bg-black">TRY 4 FREE</button>
 </div>
 <p className="text-white mt-2 font-bold">By Clicking sign up you are confirming that you agree to our terms and conditions.</p>
 

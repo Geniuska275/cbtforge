509f7e842807bb438762683aa8891588cf5b0180
Geniuska275 <aigbojie2020@gmail.com>
2024-12-27 21:21:49 +0100

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ function Hero() {
            <h2 className='text font-bold text-white text-center text-3xl mt-4 font-[Poppins]'>Multi-purpose Cloud-Based Assessment Platform</h2>
             <p className='text text-white text-center text-xl mt-3 font-[Poppins]'>We have created a multi-purpose LMS for Organizational needs.</p>
            
-            <p className=' text-white text-center text-xl mt-3 font-[Poppins]' >Your one stop LMS for School examination ,recruitment,training and scholarship tests.</p>
+            <p className='text text-white text-center text-xl mt-3 font-[Poppins]' >Your one stop LMS for School examination ,recruitment,training and scholarship tests.</p>
             <div className=' flex justify-center items-center flex-wrap gap-3 mt-[40px] ma '>
               <button className=' py-2 px-8 bg-[#7EC5FF] rounded-3xl font-bold font-[Poppins] hover:bg-black hover:border hover:text-white'>GET STARTED</button>
               <button className='py-2 px-8 bg-black border rounded-3xl font-bold font-[Poppins] text-white hover:bg-[#7EC5FF] hover:border hover:text-black'>SEE ALL PLANS</button>
